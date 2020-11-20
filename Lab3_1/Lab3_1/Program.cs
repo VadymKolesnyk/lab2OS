a05f53fed5dfe5cc106b0df8db867e4947f16a21
VadymKolesnyk <vakol2001@gmail.com>
2020-11-20 18:11:43 +0200

    Realize Analyzer class

--- Lab3_1/Lab3_1/Program.cs
+++ Lab3_1/Lab3_1/Program.cs
@@ -9,28 +9,73 @@ namespace Lab3_1
 {
     class Analyzer
     {
-        private DirectoryInfo root;
-        private uint[] barGraph;
-        private uint step;
+        private readonly DirectoryInfo root;
+        private readonly SortedDictionary<long, List<FileInfo>> barGraph;
+        private readonly uint step;
+        public uint Step { get => step; }
+        private void HandleFile(FileInfo file)
+        {
+            if (file.Exists)
+            {
+                long size = file.Length / step;
+                if (barGraph.TryGetValue(size, out List<FileInfo> files))
+                {
+                    files.Add(file);
+                }
+                else
+                {
+                    barGraph.Add(size, new List<FileInfo> { file });
+                }  
+            }
+        }
+        private void HandleDirectory(DirectoryInfo root)
+        {
+            foreach (FileInfo file in root.GetFiles())
+            {
+                HandleFile(file);
+            }
+            foreach (DirectoryInfo directory in root.GetDirectories())
+            {
+                HandleDirectory(directory);
+            }
+        }
+        public SortedDictionary<long, List<FileInfo>> Scan()
+        {
+            HandleDirectory(root);
+            foreach (var item in barGraph)
+            {
+                item.Value.Sort((left, right) =>
+                {
+                    int diff = (int)(left.Length - right.Length);
+                    return diff == 0 ? string.Compare(left.Name, right.Name) : diff;
+                });
+            }
+            return barGraph;
+        }
         public Analyzer(uint step, string path)
         {
             this.root = new DirectoryInfo(path);
             this.step = step;
+            this.barGraph = new SortedDictionary<long, List<FileInfo>>();
         }
     }
+
     class Program
     {
 
         static void Main(string[] args)
         {
-            string dirName = "C:\\Program Files";
-
-            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
-            dirInfo.
-            Console.WriteLine($"Название каталога: {dirInfo.Name}");
-            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
-            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+            Analyzer analyzer = new Analyzer(1024, @"C:\Users\Vadik\Downloads\Telegram Desktop");
+            var barGraph = analyzer.Scan();
+            foreach (var item in barGraph)
+            {
+                Console.WriteLine($"{analyzer.Step * item.Key} - { analyzer.Step * (item.Key + 1) - 1} ({item.Value.Count}):");
+                foreach (var file in item.Value)
+                {
+                    Console.WriteLine($"\t\t{file.Length,13}\t\t{file.Name}");
+                }
+            }
+            Console.ReadKey();
 
         }
     }
