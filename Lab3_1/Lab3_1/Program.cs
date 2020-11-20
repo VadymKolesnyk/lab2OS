942a553bc50b01484e8ed770c76165977edefdb1
VadymKolesnyk <vakol2001@gmail.com>
2020-11-20 16:32:41 +0200

    Initial commit

---
+++ Lab3_1/Lab3_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_1
+{
+    class Analyzer
+    {
+        private DirectoryInfo root;
+        private uint[] barGraph;
+        private uint step;
+        public Analyzer(uint step, string path)
+        {
+            this.root = new DirectoryInfo(path);
+            this.step = step;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string dirName = "C:\\Program Files";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+            dirInfo.
+            Console.WriteLine($"Название каталога: {dirInfo.Name}");
+            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+
+        }
+    }
+}
